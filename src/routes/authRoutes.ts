d0d890a7a4ae333895a7385fc4e21273afde947e
Mestre Santiago <souto.santiago@academico.ifpb.edu.br>
2024-06-04 06:44:13 -0300

    atualização de tipos

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { AuthController } from "../controllers/authController";
 const router = Router()
 const authController = new AuthController()
 
-router.post("/login", async (req, res) => {
+router.post("/login", async (req: Request, res: Response) => {
     return await authController.login(req, res)
 })
 
--- src/routes/unitRoutes.ts
+++ src/routes/unitRoutes.ts
@@ -1,20 +1,20 @@
 
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { UnitController } from "../controllers/unitController"
 
 const router = Router()
 const unitController = new UnitController()
 
 
-router.get("/:unitId", async (req, res) => {
+router.get("/:unitId", async (req: Request, res: Response) => {
     return await unitController.getById(req, res)
 })
 
-router.put("/:unitId", async (req, res) => {
+router.put("/:unitId", async (req: Request, res: Response) => {
     return await unitController.updateById(req, res)
 })
 
-router.delete("/:unitId", async (req, res) => {
+router.delete("/:unitId", async (req: Request, res: Response) => {
     return await unitController.deleteById(req, res)
 })
 export default router
